e10e4f9fd657acd9395b2ce883c9c7996e038e16
Alberto del Barrio <alberto.delbarrio.albelda@gmail.com>
2025-05-17 00:06:13 +0200

    fix: add soure map

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ciao-mondo/',
+  build: {
+    sourcemap: false, // Disable source maps to avoid the "URL constructor" error
+  },
 })
